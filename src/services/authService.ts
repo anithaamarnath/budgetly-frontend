fec45d28f71a12a67d414c0ad70a87b967f16250
anithaamarnath <anithaamarnath04@gmail.com>
2025-02-04 16:24:45 -0800

    updated auth

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -6,24 +6,11 @@ import LoginState from "../types";
 
 
 export const registerUser = async (data: FormState): Promise<void> => {
-  try {
-    const response = await axios.post("http://localhost:5000/api/signin/login", data, {
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return response.data;
-  } catch (error) {
-    console.error("Error logging in:", error);
-    throw new Error("Login failed");
-  }
+  const response = await axios.post("http://localhost:5000/api/users/register", data);
+  return response.data;
 };
 
 export const loginUser = async (data: LoginState): Promise<LoginState> => {
-  const response = await axios.post("http://localhost:5000/api/signin/login", data, {
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  return response.data; // Ensure response.data matches LoginState
+  const response = await axios.post("http://localhost:5000/api/users/signin", data);
+  return response.data;
 };
