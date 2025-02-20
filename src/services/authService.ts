385855ce0ef41cfb5b6c5c1e81b9cf780ff28c1a
anithaamarnath <anithaamarnath04@gmail.com>
2025-02-19 17:42:45 -0800

    removed console

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -7,13 +7,10 @@ const apiUrl = import.meta.env.VITE_API_URL;
 
 export const registerUser = async (data: FormState): Promise<FormState> => {
   const response = await axios.post(`${apiUrl}/api/users/register`, data);
-  console.log("Response ", response);
   return response.data;
 };
 
 export const loginUser = async (data: { email: string; password: string }): Promise<AuthState> => {
-  console.log("Login data", data);
-  
   const response = await axios.post(`${apiUrl}/api/users/signin`, data);
   return response.data;
 };
