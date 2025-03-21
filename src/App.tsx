2d91bc3b7441a5e445ce4fbd96eaf4b7934dec3a
anithaamarnath <anithaamarnath04@gmail.com>
2025-03-21 16:29:13 -0700

    app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { RootState } from "./redux/store";
 import Header from "./Header";
-import LandingPage from "./LandingPage";
 import Footer from "./Footer/Footer";
+import SidebarComponent from "./Sidebar/Sidebar";
+import LandingPage from "./LandingPage";
 import LoginPage from "./LoginPage";
-import { Container, MainContent } from "./styles/styled";
 import SignupForm from "./SignupForm";
 import Dashboard from "./Dashboard";
 import NewEntryForm from "./NewEntryForm";
 import PrivateRoute from "./components/PrivateRoute";
+import { Container, MainContent, RightContainer } from "./styles/styled";
 
 const App: React.FC = () => {
+  const [sidebarExpanded, setSidebarExpanded] = useState(false);
+  const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
   return (
     <Router>
       <Container>
         <Header />
-        <MainContent>
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupForm />} />
-
-            <Route
-              path="/dashboard"
-              element={
-                <PrivateRoute>
-                  <Dashboard />
-                </PrivateRoute>
-              }
+        {/* Show Sidebar & MainContent only if logged in */}
+        {isLoggedIn ? (
+          <>
+            <SidebarComponent
+              expanded={sidebarExpanded}
+              setExpanded={setSidebarExpanded}
             />
-            <Route
-              path="/addNew"
-              element={
-                <PrivateRoute>
-                  <NewEntryForm />
-                </PrivateRoute>
-              }
-            />
-          </Routes>
-        </MainContent>
+            <RightContainer expanded={sidebarExpanded}>
+              <MainContent>
+                <Routes>
+                  <Route path="/" element={<LandingPage />} />
+                  <Route
+                    path="/dashboard"
+                    element={
+                      <PrivateRoute>
+                        <Dashboard />
+                      </PrivateRoute>
+                    }
+                  />
+                  <Route
+                    path="/addNew"
+                    element={
+                      <PrivateRoute>
+                        <NewEntryForm />
+                      </PrivateRoute>
+                    }
+                  />
+                </Routes>
+              </MainContent>
+            </RightContainer>
+          </>
+        ) : (
+          // Show only Landing Page when logged out
+          <MainContent>
+            <Routes>
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupForm />} />
+            </Routes>
+          </MainContent>
+        )}
         <Footer />
       </Container>
     </Router>
